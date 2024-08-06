dfa1e290c93b23ea0b1106cd5c4ff9a4390ae59e
CarlosViniciusSantos <carlos.vinicius1@aluno.ifsp.edu.br>
2024-08-06 17:46:06 -0300

    add routes: auth and account

--- src/server.js
+++ src/server.js
@@ -2,15 +2,32 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.json(
-        {
-            message: "Olá Mundo Express!",
-            success: "Sucesso ao executar"
-        }
-    )
+app.post('/auth/signup', (req, res) =>{
+    res.json({message: "Rota de POST Auth/singup"})
+})
+app.post('/auth/login', (req, res) =>{
+    res.json({message: "Rota de POST Auth/login"})
+})
+app.post('/auth/logout', (req, res) =>{
+    res.json({message: "Rota de POST Auth/logout"})
 })
 
-app.listen(3000, () => {
+app.post('/account', (req, res) =>{
+    res.json({message: "Rota de POST account"})
+})
+app.get('/account/list', (req, res) =>{
+    res.json({message: "Rota de GET account/list"})
+})
+app.get('/account/:id', (req, res) =>{
+    res.json({message: "Rota de GET account/:ID"})
+})
+app.put('/account/:id', (req, res) =>{
+    res.json({message: "Rota de PUT account/:ID"})
+})
+app.delete('/account/:id', (req, res) =>{
+    res.json({message: "Rota de DELETE account/:ID"})
+})
+
+app.listen(3000,() => {
     console.log('Servidor rodando em http://localhost:3000')
 })
