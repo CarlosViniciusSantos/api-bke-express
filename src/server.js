5521cfa15c33e6ab36d085e64fe374d92e9daf35
CarlosViniciusSantos <carlos.vinicius1@aluno.ifsp.edu.br>
2024-08-06 15:52:27 -0300

    add lib express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log('Olá mundo!')
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.json(
+        {
+            message: "Olá Mundo Express!",
+            success: "Sucesso ao executar"
+        }
+    )
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000')
+})
