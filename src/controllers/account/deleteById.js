10fab654325e677bf27d1334c53209a9b6f50514
CarlosViniciusSantos <carlos.vinicius1@aluno.ifsp.edu.br>
2024-08-27 17:53:40 -0300

    add prisma and list, getbyid, delete account

--- src/controllers/account/deleteById.js
+++ src/controllers/account/deleteById.js
@@ -1,5 +1,18 @@
-const del =  (req, res) =>{
-    res.json({message: "Rota de DELETE account/:ID"})
+import { deleteByIdAccount } from "../../models/accountModel.js"
+
+const del = async (req, res) =>{
+    const {id} = req.params
+    const account = await deleteByIdAccount(+id)
+
+    if (!account)
+        return res.status(404).json({
+            error: `Conta com id ${id}, não existente!`
+        })
+
+    return res.json({
+        message: "Conta deletada com sucesso",
+        account
+    })
 }
 
 export default del
--- src/controllers/account/listController.js
+++ src/controllers/account/listController.js
@@ -1,5 +1,11 @@
-const list =  (req, res) =>{
-    res.json({message: "Rota de GET account/list TESTE DEPLOY"})
+import { listAccounts } from "../../models/accountModel.js"
+
+const list =  async (req, res) =>{
+    const accounts = await listAccounts()
+    return res.json({
+        message: "Contas Listasdas com sucesso!",
+        accounts
+    })
 }
 
 export default list
