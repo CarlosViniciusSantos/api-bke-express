6591e5f17d3fbff5e3837f72286b60e93f98ebb1
CarlosViniciusSantos <carlos.vinicius1@aluno.ifsp.edu.br>
2024-09-03 17:51:53 -0300

    add signup

---
+++ src/models/authModel.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+export const create = async (user) => {
+    const result = await prisma.user.create({
+        data: user
+    })
+    return result
+}
