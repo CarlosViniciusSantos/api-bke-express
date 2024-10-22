4c196fd69b875737d902fac3e71d2d3c72d9e0b7
CarlosViniciusSantos <carlos.vinicius1@aluno.ifsp.edu.br>
2024-10-22 17:52:54 -0300

    add signup route

--- src/models/authModel.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient()
-
-export const create = async (user) => {
-    const result = await prisma.user.create({
-        data: user
-    })
-    return result
-}
